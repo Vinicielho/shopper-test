5087884dc52b65d4635e9e06fb25398a5de2266b
vinicius <viniciuscoelho1995@yahoo.com.br>
2024-08-28 15:14:07 -0300

    feature: docker-compose is launching the application

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,21 +1,31 @@
 import Fastify from "fastify";
+import { Client } from "pg";
 
-const fastify = Fastify({
-  logger: true,
-});
+const fastify = Fastify({ logger: true });
+const client = new Client({ connectionString: process.env.DATABASE_URL });
 
-fastify.get("/", async (request, reply) => {
-  return { hello: "world" };
-});
+const host =
+  process.env.BACKEND_HOST ||
+  (() => {
+    throw new Error("Environment variable BACKEND_HOST is not defined");
+  })();
 
-const start = async () => {
-  try {
-    await fastify.listen({ port: 3000 });
-    console.log("Server listening on http://localhost:3000");
-  } catch (err) {
+const port = parseInt(
+  process.env.BACKEND_PORT ||
+    (() => {
+      throw new Error("Environment variable BACKEND_PORT is not defined");
+    })(),
+  10
+);
+
+fastify.listen({ host, port }, (err, address) => {
+  if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-};
+  fastify.log.info(`Server listening at ${address}`);
+});
 
-start();
+fastify.get("/", async (request, reply) => {
+  return "hi, i'm here!";
+});
